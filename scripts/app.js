6c4f0b5e1d0ee829278e72eaad0f44dc6fd3b058
LeXteRpro <lexter@hotmail.ca>
2016-04-19 23:02:29 -0400

    Banner size, extra text, link

--- scripts/app.js
+++ scripts/app.js
@@ -24,8 +24,8 @@
     //CreateJS Section
 
     // Global Variables
-    var screenWidth = 468;
-    var screenHeight = 200;
+    var screenWidth = 250;
+    var screenHeight = 250;
 
     // References
     var canvas = document.getElementById("canvas");
@@ -36,10 +36,16 @@
     var stage = new createjs.Stage(canvas);
 
     var helloLabel = null;
+
+    var website = null;
+
+    var viewHere = null;
+
     var helloLabelMove = 1;
 
     var button = null;
     var buttonMove = 0;
+
 
     function init() {
         console.log("Testing");
@@ -57,13 +63,11 @@
 
     // Run every Frame
     function animationLoop() {
-        
-
         helloLabel.x += helloLabelMove;
         button.x += buttonMove;
 
 
-        if ((helloLabel.x >= screenWidth) || (helloLabel.x <= 270)) {
+        if ((helloLabel.x >= screenWidth - 20) || (helloLabel.x <= 160)) {
             helloLabelMove *= -1;
         }
         
@@ -82,12 +86,29 @@
         button.y = screenHeight * 0.5;
         stage.addChild(button);
 
-        helloLabel = new createjs.Text("See Work", "30px Consolas", "white");
+        helloLabel = new createjs.Text("See Work", "40px Consolas", "white");
         helloLabel.regX = helloLabel.getMeasuredWidth() * 0.9;
         helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
         helloLabel.x = screenWidth * 0.7;
-        helloLabel.y = screenHeight * 0.5;
+        helloLabel.y = screenHeight * 0.3;
         stage.addChild(helloLabel);
+
+
+        website = new createjs.Text("New Front End Projects", "19px Consolas", "yellow");
+        website.regX = website.getMeasuredWidth() * 0.9;
+        website.regY = website.getMeasuredHeight() * 0.5;
+        website.x = screenWidth * 0.85;
+        website.y = screenHeight * 0.6;
+        stage.addChild(website);
+
+        viewHere = new createjs.Text("View Here", "35px Consolas", "yellow");
+        viewHere.regX = viewHere.getMeasuredWidth() * 0.9;
+        viewHere.regY = viewHere.getMeasuredHeight() * 0.5;
+        viewHere.x = screenWidth * 0.8;
+        viewHere.y = screenHeight * 0.8;
+        stage.addChild(viewHere);
+
+
 
         button.on("click", function() {
             if(helloLabel.text === "Clicked!") {
@@ -102,12 +123,13 @@
         });
 
         button.on('mouseover', function() {
-            button.alpha = 0.5;
+            button.alpha = 0.2;
         })
 
         button.on('mouseout', function() {
-            button.alpha = 1;
+            button.alpha = 0.45
         })
+        button.alpha = 0.5;
     }
 
     window.onload = init;
